65bcff47080b931dcc33fe34d57ec50ababd58cd
Clayton Haight <clayhaight@yahoo.ca>
2023-12-06 03:23:46 -0500

    Update day6a.js

--- day_06/day6a.js
+++ day_06/day6a.js
@@ -1,20 +1,25 @@
-f = "day_06/day6_input.txt"
-lines = open(f).read().splitlines()
+const fs = require('fs');
 
-time_data = list(map(int, lines[0].split()[1:]))
-distance_data = list(map(int, lines[1].split()[1:])) 
-tds = list(zip(time_data, distance_data))
+// Read file and split into lines
+const f = "day_06/day6_input.txt";
+const lines = fs.readFileSync(f, 'utf8').split('\n');
 
-num_ways = []
-for td in tds:
-    ways = 0
-    for v in range(td[0]+1):
-        d = v*(td[0]-v)
-        if d > td[1]:
-            ways += 1
-    num_ways.append(ways)
-print(num_ways)
-result = 1
-for val in num_ways:
-    result *= val
-print(result)
+// Extract and convert time and distance data
+const timeData = lines[0].split(' ').slice(1).map(Number);
+const distanceData = lines[1].split(' ').slice(1).map(Number);
+const tds = timeData.map((t, i) => [t, distanceData[i]]);
+
+let numWays = tds.map(td => {
+    let ways = 0;
+    for (let v = 0; v <= td[0]; v++) {
+        let d = v * (td[0] - v);
+        if (d > td[1]) ways++;
+    }
+    return ways;
+});
+
+console.log(numWays);
+
+// Calculate the final result
+let result = numWays.reduce((acc, val) => acc * val, 1);
+console.log(result);
